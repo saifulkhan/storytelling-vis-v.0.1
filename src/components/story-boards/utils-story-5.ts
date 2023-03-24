c49918737957e9022e4ddd7bc512330ba3e37cf6
saifulkhan <saiful.etc@gmail.com>
2023-03-24 14:32:50 +0000

    refactor - 4

--- src/components/story-boards/utils-story-5.ts
+++ src/components/story-boards/utils-story-5.ts
@@ -2,7 +2,7 @@ import { readCSVFile } from "./utils-data";
 import { findDateIdx } from "./utils-feature-detection";
 import { AnimationType } from "src/models/AnimationType";
 import { MirroredBarChart } from "./MirroredBarChart";
-import { TimeSeriesPlot } from "./TimeSeries_new";
+import { TimeSeriesPlot } from "./TimeSeriesPlot";
 import { GraphAnnotation, TSPAnnotation } from "./GraphAnnotation";
 import { Color } from "./Colors";
 import { DotColor, TextColor, FeatureType } from "./FeatureAndColorMap";
